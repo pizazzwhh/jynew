677bddef6789ad71656111991f9c8230de6a7df7
pizazzwhh <pizazzwhh@qq.com>
2023-02-27 22:25:15 +0800

    添加jhz的mod文件

---
+++ jyx2/Assets/Mods/jhz/Lua/1.lua
@@ -0,0 +1,33 @@
+Talk(1, "你要和我一个人打吗");
+if AskBattle() == true then
+    goto danTiao
+else
+    Talk(1, "那你是要打我一队？！");
+    if AskBattle() == true then
+        goto qunOu
+    else
+        Talk(1, "哈，怕了吧")
+        do return end;
+    end
+end;
+
+::danTiao::
+
+if TryBattle(0) == true then
+    ShowMessage("单挑赢了");
+    AddItem(174,3);
+else
+    ShowMessage("单挑输了");
+end;
+do return end;
+
+::qunOu::
+
+if TryBattle(60) == true then
+    ShowMessage("群殴赢了");
+    AddRepute(10);
+    AddItem(174,3);
+else
+    ShowMessage("群殴输了");
+end;
+do return end;
---
+++ jyx2/Assets/Mods/jhz/Lua/28.lua
@@ -0,0 +1,15 @@
+Talk(1, "你好", "talkname1", 0);
+Talk(0, "你好", "talkname0", 1);
+if AskBattle() == true then goto label0 end;
+    do return end;
+::label0::
+    Talk(0, "晚辈斗胆向前辈讨教讨教。", "talkname0", 1);
+    if TryBattle(27) == false then goto label1 end;
+        LightScence();
+        Talk(1, "可恨啊！可恨！只恨胡某刀谱不全，未能练成我祖传之胡家刀法……", "talkname1", 0);
+        do return end;
+::label1::
+        LightScence();
+        Talk(1, "小兄弟，功夫虽有精进，但火候仍嫌不够。", "talkname1", 0);
+        Talk(0, "他日再向胡大哥领教刀法。", "talkname0", 1);
+do return end;
