c879b53bae87567928d85c280a20127a25fd2355
sha'shuo <827408106@qq.com>
2023-03-05 21:24:24 +0800

    lua完善1.0
    完善lua

--- jyx2/Assets/Mods/jhz/Lua/12.lua
+++ jyx2/Assets/Mods/jhz/Lua/12.lua
@@ -1,15 +1,33 @@
-Talk(1, "你好", "talkname1", 0);
-Talk(0, "你好", "talkname0", 1);
-if AskBattle() == true then goto label0 end;
-    do return end;
-::label0::
-    Talk(0, "晚辈斗胆向前辈讨教讨教。", "talkname0", 1);
-    if TryBattle(11) == false then goto label1 end;
-        LightScence();
-        Talk(1, "可恨啊！可恨！只恨胡某刀谱不全，未能练成我祖传之胡家刀法……", "talkname1", 0);
+Talk(13, "你要和我一个人打吗");
+if AskBattle() == true then
+    goto danTiao
+else
+    Talk(13, "那你是要打我一队？！");
+    if AskBattle() == true then
+        goto qunOu
+    else
+        Talk(13, "哈，怕了吧")
         do return end;
-::label1::
-        LightScence();
-        Talk(1, "小兄弟，功夫虽有精进，但火候仍嫌不够。", "talkname1", 0);
-        Talk(0, "他日再向胡大哥领教刀法。", "talkname0", 1);
+    end
+end;
+
+::danTiao::
+
+if TryBattle(11) == true then
+    ShowMessage("单挑赢了");
+    AddItem(174,3);
+else
+    ShowMessage("单挑输了");
+end;
+do return end;
+
+::qunOu::
+
+if TryBattle(71) == true then
+    ShowMessage("群殴赢了");
+    AddRepute(10);
+    AddItem(174,3);
+else
+    ShowMessage("群殴输了");
+end;
 do return end;
